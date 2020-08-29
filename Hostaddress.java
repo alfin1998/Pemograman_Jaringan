898644449885fb5fa3539a905bee256e71414f0b
M. A Z <alfinyuki@gmail.com>
2020-08-29 12:13:50 +0700

    Update Hostaddress.java

--- Hostaddress.java
+++ Hostaddress.java
@@ -4,7 +4,7 @@ public class Praktikum2 {
    public static void main(String[] args)
 {
 try {
-      InetAddress inet1 = InetAddress.getByName("www.polinema.ac.id");
+      InetAddress inet1 = InetAddress.getByName("www.polinema.ac.id"); //Host address
       System.out.println("HostAddress=" + inet1.getHostAddress());
       InetAddress inet2 = InetAddress.getByName("192.168.134.134");
       System.out.println("HostName=" + inet2.getHostName());
